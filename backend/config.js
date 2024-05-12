3c70ee6ed3f04cb3bb0be6dc0f3e4af49ba9a2aa
matheinz <matthew_heinz_sanchez@dlsu.edu.ph>
2024-05-13 01:21:34 +0800

    added another note in config, specifically about the mongoDB URL

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,4 @@
 export const PORT = 3000;
 export const mongodbURL = "mongodb+srv://mhos:accDB@accountsdb.edcasew.mongodb.net/accounts?retryWrites=true&w=majority&appName=accountsDB";
+// in the mongodb URL "mongodb+srv://mhos:accDB@accountsdb.edcasew.mongodb.net/     accounts    ?retryWrites=true&w=majority&appName=accountsDB"
+//                                                                                     ^^^ pls insert the database name from mongoDB (u got stuck here for 3 hrs)
